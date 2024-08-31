345a48540f605198ccc04075fd74e1ae7a73cc4a
Lalit Kakkar <lalitkakkar50@gmail.com>
2024-08-31 17:20:00 +0530

    cookie fix

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -71,10 +71,17 @@ const handleLoginUser = async (req, res) => {
     // removing password field from last output
     const { password: pass, ...rest } = user._doc;
 
-    return res
-      .status(200)
-      .cookie("token", token, { httpOnly: true })
-      .json({ success: true, data: rest, message: "login successfull" });
+    return (
+      res
+        .status(200)
+        // .cookie("token", token, { httpOnly: true })
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "None",
+        })
+        .json({ success: true, data: rest, message: "login successfull" })
+    );
   } catch (error) {
     return res.status(500).json({
       success: false,
