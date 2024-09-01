eebe308c3ee4f157e8950b6420754cb29705f218
Lalit Kakkar <lalitkakkar50@gmail.com>
2024-09-01 17:34:32 +0530

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
 
 // routes
+app.use("/api/", document.write("Welcome to Mern  Blog App"));
 app.use("/api/user", userRoute);
 app.use("/api/blog", blogRoute);
 app.use("/api/comment", commentRouter);
