8a20773e6e26980e9f3af2ea8cefff389f14c6e6
Lalit Kakkar <lalitkakkar50@gmail.com>
2024-09-01 18:08:03 +0530

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -16,11 +16,9 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    SameSite: "none",
-    secure: true,
   })
 );
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
