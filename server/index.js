40f450892092df32256c1a87d8773071b534a6b9
Lalit Kakkar <lalitkakkar50@gmail.com>
2024-09-02 10:49:23 +0530

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { userRoute, blogRoute, commentRouter } from "./routes/index.js";
-import path from "path";
 
 dotenv.config({
   path: "./.env",
@@ -23,13 +22,6 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 // routes
 app.use("/api/user", userRoute);
